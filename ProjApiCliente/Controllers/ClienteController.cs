a1cf20a450300976e509935cd7407a2ec147cdbc
ericserra <ericserra.souza@gmail.com>
2021-03-31 10:17:42 -0300

    commit init again

--- ProjApiCliente/Controllers/ClienteController.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjApiCliente.Controllers
-{
-    public class ClienteController
-    {
-        
-    }
-}
--- ProjApiCliente/Dal/ApiClienteContext.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjApiCliente.Dal
-{
-    public class ApiClienteContext
-    {
-        
-    }
-}
